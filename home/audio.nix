8945e35f9effc19adde5de3aa2d153eac716d967
Brian Hechinger <wonko@4amlunch.net>
2026-06-10 12:24:42 +0100

    more audio nonsense

--- home/audio.nix
+++ home/audio.nix
@@ -36,6 +36,54 @@ let
       }
     ]
   '';
+  ardourDefaultRelink = pkgs.writeShellScript "ardour-default-relink" ''
+    set -u
+
+    pw_link="${pkgs.pipewire}/bin/pw-link"
+    grep="${pkgs.gnugrep}/bin/grep"
+    sleep="${pkgs.coreutils}/bin/sleep"
+
+    has_output() {
+      "$pw_link" -o | "$grep" -Fxq "$1"
+    }
+
+    has_input() {
+      "$pw_link" -i | "$grep" -Fxq "$1"
+    }
+
+    wait_for_graph() {
+      count=0
+      while [ "$count" -lt 40 ]; do
+        if has_output "ardour:Master/audio_out 1" \
+          && has_input "alsa_output.firewire-0x00130e0401c04de0.multichannel-output:playback_FL" \
+          && has_input "ardour:MIDI Control In"; then
+          return 0
+        fi
+        count=$((count + 1))
+        "$sleep" 0.5
+      done
+      return 0
+    }
+
+    link() {
+      "$pw_link" "$1" "$2" 2>/dev/null || true
+    }
+
+    wait_for_graph
+
+    link "System Sounds:monitor_FL" "ardour:System/audio_in 1"
+    link "System Sounds:monitor_FR" "ardour:System/audio_in 2"
+    link "Games:monitor_FL" "ardour:Games/audio_in 1"
+    link "Games:monitor_FR" "ardour:Games/audio_in 2"
+    link "Music:monitor_FL" "ardour:Music/audio_in 1"
+    link "Music:monitor_FR" "ardour:Music/audio_in 2"
+
+    link "alsa_input.firewire-0x00130e0401c04de0.multichannel-input:capture_AUX0" "ardour:Mic/audio_in 1"
+    link "Midi-Bridge:nanoKONTROL2: _ CTRL (capture)" "ardour:MIDI Control In"
+
+    link "ardour:Master/audio_out 1" "alsa_output.firewire-0x00130e0401c04de0.multichannel-output:playback_FL"
+    link "ardour:Master/audio_out 2" "alsa_output.firewire-0x00130e0401c04de0.multichannel-output:playback_FR"
+  '';
 in
 {
   home.packages = with pkgs; [
@@ -63,13 +111,22 @@ in
         "pipewire.service"
         "wireplumber.service"
       ];
-      PartOf = [ "hyprland-session.target" ];
+      PartOf = [
+        "hyprland-session.target"
+        "pipewire.service"
+        "wireplumber.service"
+      ];
     };
 
     Service = {
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
       ExecStart = "${ardourPipewire}/bin/ardour9 /home/wonko/Default";
+      ExecStartPost = "${ardourDefaultRelink}";
+      KillSignal = "SIGINT";
       Restart = "on-failure";
       RestartSec = 5;
+      SuccessExitStatus = "SIGINT";
+      TimeoutStopSec = 120;
     };
 
     Install.WantedBy = [ "hyprland-session.target" ];
