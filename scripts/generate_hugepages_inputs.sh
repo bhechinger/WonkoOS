12d1063cc32441e1c8d3ecb7cdf839748c0420ac
Brian Hechinger <wonko@4amlunch.net>
2026-06-12 10:23:02 +0100

    updated hugepages generation to make it not drift the outputs

--- scripts/generate_hugepages_inputs.sh
+++ scripts/generate_hugepages_inputs.sh
@@ -6,7 +6,7 @@ output="${1:-systems/${host_name}/hugepages-inputs.nix}"
 
 kernel_release="$(uname -r)"
 huge_page_size_kib="$(awk '/Hugepagesize/ { print $2; exit }' /proc/meminfo)"
-segment_sizes="$(awk 'NR > 1 && $4 ~ /^[0-9]+$/ { printf "%s ", $4 }' /proc/sysvipc/shm)"
+segment_sizes="$(awk 'NR > 1 && $4 ~ /^[0-9]+$/ { print $4 }' /proc/sysvipc/shm | sort -n | tr '\n' ' ')"
 
 mkdir -p "$(dirname "$output")"
 tmp="$(mktemp "${output}.tmp.XXXXXX")"
