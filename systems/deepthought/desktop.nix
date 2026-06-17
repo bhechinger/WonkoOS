7ad84e01b81af90ea01953b5f0e3e4a79d7307b5
Brian Hechinger <wonko@4amlunch.net>
2026-06-17 19:57:27 +0100

    document kdeconnect firewall behavior

--- systems/deepthought/desktop.nix
+++ systems/deepthought/desktop.nix
@@ -6,6 +6,8 @@
       withUWSM = true;
       xwayland.enable = true;
     };
+    # The NixOS kdeconnect module opens the required TCP/UDP 1714-1764
+    # firewall ranges when enabled.
     kdeconnect.enable = true;
     evince.enable = true;
   };
