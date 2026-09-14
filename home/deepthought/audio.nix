da5919180cd1226594ddc70a3024dab1d93d0973
Brian Hechinger <wonko@4amlunch.net>
2026-09-14 19:26:26 +0100

    fix(audio): autostart Ardour with ALSA

--- home/deepthought/audio.nix
+++ home/deepthought/audio.nix
@@ -1,5 +1,20 @@
-{ pkgs, ... }:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  home.activation.useArdourAlsa = lib.hm.dag.entryBetween [ "reloadSystemd" ] [ "writeBoundary" ] ''
+    ARDOUR_CONFIG=${lib.escapeShellArg "${config.xdg.configHome}/ardour9/config"}
+    if test -f "$ARDOUR_CONFIG"; then
+      ${pkgs.gnused}/bin/sed -E -i \
+        -e '/<State backend=/ s/active="[01]"/active="0"/' \
+        -e '/<State backend="ALSA"/ s/active="0"/active="1"/' \
+        "$ARDOUR_CONFIG"
+    fi
+  '';
+
   home.packages = with pkgs; [
     carla
     qpwgraph
@@ -47,6 +62,26 @@
       ];
       settings.Keywords = "PipeWire;MIDI;ALSA;Qt;";
     };
+  };
+
+  systemd.user.services.ardour-default = {
+    Unit = {
+      Description = "Ardour Default session";
+      Wants = [ "wireplumber.service" ];
+      After = [ "wireplumber.service" ];
+      PartOf = [ "hyprland-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.ardour}/bin/ardour9 /home/wonko/Default";
+      KillSignal = "SIGINT";
+      Restart = "on-failure";
+      RestartSec = 5;
+      SuccessExitStatus = "SIGINT";
+      TimeoutStopSec = 120;
+    };
+
+    Install.WantedBy = [ "hyprland-session.target" ];
   };
 
   services.spotify-midi-control = {
